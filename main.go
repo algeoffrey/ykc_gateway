72c409cdff53154d39494f9e0d6836547e325487
unknown <joshua@tebengan.com>
2025-01-09 16:58:49 +0700

    start charging debugging

--- main.go
+++ main.go
@@ -281,7 +281,7 @@ func drain(opt *Options, conn net.Conn) error {
 }
 func sendMessage(conn net.Conn, message []byte) error {
 	// Convert message to bytes or proper format
-	fmt.Printf("Hex: %s\n", message)
+	PrintHexAndByte(message)
 	// Send the message to the device
 	_, err := conn.Write(message)
 	if err != nil {
